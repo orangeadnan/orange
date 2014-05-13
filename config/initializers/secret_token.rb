dc02395e346536cbf1b013c44b674dfc13eed9da
Orangeadnan <orangeartworld@gmail.com>
2014-05-12 20:02:42 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Orange::Application.config.secret_key_base = '6e4977a33536a433e2c5ff3839e165dc9809adfb5599ce0e22b65a00abc7f59797c9be619bd22a601d1ff99fa5a330b77ad2db62164b1479ce06af03a25e61c2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Orange::Application.config.session_store :cookie_store, key: '_orange_session'
